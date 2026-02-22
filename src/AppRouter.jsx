9dca571ad6f197203c1ebd0c69b25c24618243d4
rajarajan08 <rajarajan.rn7@gmail.com>
2026-02-22 10:48:55 +0530

    Home

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 
 export default function AppRouter() {
   return (
